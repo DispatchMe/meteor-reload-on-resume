0e4f2fbdfff3312d2ff80bba32ed19cc5762702b
kellyje <kellyje@outlook.com>
2015-05-01 04:19:13 -0400

    Add check around splashscreen object
    Minor cleanup.
    From: https://gist.github.com/jperl/60d02a5d4301c1e29eec

--- reload_on_resume.js
+++ reload_on_resume.js
@@ -6,19 +6,21 @@ var hasResumed = false;
 var retryReloadFunc = null;
 
 var retryReloadOnResume = function () {
-  // Set resumed to true so _onMigrate performs the reload
+  // Set hasResumed to true so _onMigrate performs the reload
   hasResumed = true;
 
   // Show the splashscreen during the reload
-  // it will be hidden when the app starts
-  navigator.splashscreen.show();
+  // it will hide when the app starts
+  if (navigator.splashscreen) {
+    navigator.splashscreen.show();
+  }
 
   // Re-run the onMigrate hooks
   retryReloadFunc();
 };
 
-Reload._onMigrate(function (retryReload) {
-  retryReloadFunc = retryReload;
+Reload._onMigrate(function (retry) {
+  retryReloadFunc = retry;
 
   // Prevent duplicate listeners in case _onMigrate is called multiple times
   document.removeEventListener("resume", retryReloadOnResume, false);
@@ -27,6 +29,6 @@ Reload._onMigrate(function (retryReload) {
     document.addEventListener("resume", retryReloadOnResume, false);
   }
 
-  // Reload the app if we have resumed
+  // Reload the app if we resumed
   return [hasResumed];
 });
